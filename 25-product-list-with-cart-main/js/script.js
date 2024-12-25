2994b734285468c6edbe473dacdc4708562b3506
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-25 17:12:13 -0300

    increment and decrement functions added

--- 25-product-list-with-cart-main/js/script.js
+++ 25-product-list-with-cart-main/js/script.js
@@ -2,6 +2,10 @@ const btnsAddCart = document.getElementsByClassName("add-cart");
 const dessert = document.querySelectorAll(".image-dessert img");
 const offs = document.querySelectorAll(".add-cart .off");
 const ons = document.querySelectorAll(".add-cart .on");
+const pluss = document.getElementsByClassName("fa-plus");
+const minuss = document.getElementsByClassName("fa-minus");
+const qtdItems = document.getElementsByClassName("qtd-item");
+const counts = Array(btnsAddCart.length).fill(1);
 
 Array.from(btnsAddCart).forEach((btnAddCart, indice) => {
   btnAddCart.addEventListener("click", function() {
@@ -9,5 +13,25 @@ Array.from(btnsAddCart).forEach((btnAddCart, indice) => {
     btnAddCart.classList.toggle("selected-background");
     offs[indice].classList.toggle("oculto");
     ons[indice].classList.toggle("oculto");
+  })
+});
+
+Array.from(pluss).forEach((plus, indice) => {
+  plus.addEventListener("click", function(event) {
+    event.stopPropagation();
+    counts[indice]++;
+    qtdItems[indice].textContent = counts[indice];
+  })
+});
+
+Array.from(minuss).forEach((minus, indice) => {
+  minus.addEventListener("click", function(event) {
+    event.stopPropagation();
+    if(counts[indice]>1) {
+      counts[indice]--;
+      qtdItems[indice].textContent = counts[indice];
+    } else {
+      alert("Não podem valores menores que zero.");
+    }
   })
 });
